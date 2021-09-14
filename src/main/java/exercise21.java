687428a8fb73996d27e638d258e6a5378ffe2e37
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 23:53:34 -0400

    Commit

--- src/main/java/exercise21.java
+++ src/main/java/exercise21.java
@@ -8,5 +8,59 @@ import java.util.Scanner;
 public class exercise21 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int month;
+        String name = "";
+        System.out.println("Please enter the number of the month:");
+        month = input.nextInt();
+        switch (month){
+            case 1:
+                name = "January";
+            break;
+
+            case 2:
+                name = "February";
+            break;
+
+            case 3:
+                name = "March";
+            break;
+
+            case 4:
+                name = "April";
+            break;
+
+            case 5:
+                name = "May";
+            break;
+
+            case 6:
+                name = "June";
+            break;
+
+            case 7:
+                name = "July";
+            break;
+
+            case 8:
+                name = "August";
+            break;
+
+            case 9:
+                name = "September";
+            break;
+
+            case 10:
+                name = "October";
+            break;
+
+            case 11:
+                name = "November";
+            break;
+
+            case 12:
+                name = "December";
+            break;
+        }
+        System.out.printf("The name of the month is %s.", name);
     }
 }
